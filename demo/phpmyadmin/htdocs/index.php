ea4689983d460167742fb0774650e9afa8403b3e
Pranith Kumar Pola <ppola@bgsu.edu>
2018-09-16 23:47:52 -0400

    added index.php in htdocs

---
+++ demo/phpmyadmin/htdocs/index.php
@@ -0,0 +1,45 @@
+<html>
+ <head>
+  <h1><center><title>Assignment 1 - Garimella, Bhuvana</title></center></h1>
+  <meta charset="UTF-8">
+  <meta name="description" content="Bhuvana Garimella's Bio">
+  <meta name="keywords" content="HTML">
+  <meta name="author" content="Bhuvana Garimella">
+ </head>
+ <?php phpinfo(); ?>
+ <body>
+ <div>
+    <h2>Summary</h2>
+<p> I am a junior, Computer Science major at BGSU. I am from India. I am passionate about technology. I am currently interning with Electric Quilt company as a student programmer. App development is something which interests me so much. I love pets. I am from India.
+
+  </p>
+  </div>
+  <div>
+  <h2>Personal Information</h2>
+  <p>
+   <li> Name: Bhuvana Garimella</li>
+   <li> Address: 505 Clough St Apt A3, Bowling Green, OH 43402</li>
+   <li> Phone Number: 567-970-6375</li>
+   <li> email address: bgarime@bgsu.edu</li>
+   </p>
+   </div> 
+     <div>
+        <h2>Academic Information</h2>
+   <p> 
+    Academic Information: I am currently a Junior. I am taking 20 credit hours this semester. I recently declared my minor as Data Science as I am finding it very interesting. I am currently enrolled in CS 3350, MATH 3430, CS 3140, CS 3080 PHYS 2020 and CS 3900.
+</p>
+</div>
+  
+  <div>
+  <h2>Employment Information</h2>
+  <p>
+
+<li>Aug 2016- July 2018: Library Assistant- Browne Popular Culture Library</li>
+<li>May 2018 - Present : Software programmer intern- The Electric Wuilt Company</li>
+<li>May 2018 - Present : Career Center Student Ambassador</li>
+
+
+  </p>
+  </div>    
+ </body>
+</html>
